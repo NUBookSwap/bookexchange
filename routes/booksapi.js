62d232f5db8e1e313df181fea414467ec2897204
Brenda C. Cheung <bcc8706@fsm.northwestern.edu>
2018-06-02 10:50:58 -0500

    Bookswap get routes

--- routes/booksapi.js
+++ routes/booksapi.js
@@ -1,5 +1,22 @@
 var db = require("../models");
 
-module.exports = function(app){
-    // Our get, put, post, and delete requests to the books api here
+module.exports = function (app) {
+    // Our get, put, post, and delete requests to the books api here 
+    app.get("/api/books", function (req, res) {
+        var query= {};
+
+        db.Books.findAll({
+            where: query,
+            include: [ db.User]    
+        }).then(function (results) {
+            res.json(results);
+        });
+    });
+
+    // POST route for saving a new posts
+    app.post("/api/books", function(req, res) {
+        db.Books.create(req.body).then(function(results){
+            res.json(results);
+        });
+    });
 }
