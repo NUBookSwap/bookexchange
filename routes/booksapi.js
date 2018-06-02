3f2f1d0f85fa2694b3ff34c7dac9652b795edeb4
Olubukola Y. Ogundipe <buki@Olubukolas-MacBook-Pro.local>
2018-06-02 12:45:43 -0500

    updated the post route for the add page

--- routes/booksapi.js
+++ routes/booksapi.js
@@ -15,7 +15,26 @@ module.exports = function (app) {
 
     // POST route for saving a new posts
     app.post("/api/books", function(req, res) {
-        db.Books.create(req.body).then(function(results){
+        var UserId = req.user.id;
+        var name = req.user.firstname;
+        var email = req.user.email;
+        var image = req.body.image;
+        var title = req.body.title;
+        var price = req.body.price;
+        var condition = req.body.condition;
+        var description = req.body.description;
+        var newBook = {
+            image: image,
+            title: title,
+            price: price,
+            condition: condition,
+            description: description,
+            UserId: UserId,
+            name: name,
+            email: email
+        }
+
+        db.Books.create(newBook).then(function(results){
             res.json(results);
         });
     });
