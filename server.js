18c88f9841947ae1e991e3865b4ed05850bb7039
Benjamin Miller <benjamin.miller2@northwestern.edu>
2018-06-09 12:34:17 -0500

    heroku again

--- server.js
+++ server.js
@@ -61,8 +61,8 @@ app.listen(PORT, function(err){
     }
 });
 
-if(process.env.JAWSBD_URL){
-    connection = mysql.createConnection(process.env.JAWSBD_URL);
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
     connection = mysql.createConnection({
         host: 'localhost',
